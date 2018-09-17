ccc59d22f3abb8055bd6efcb5b1c6435c2f812ff
demian sims <demiansims@gmail.com>
2018-09-17 12:49:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
@@ -43,3 +44,40 @@ function removeElementFromEndOfArray(array, element) {
 
 
 
+=======
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittlesE'];
+
+function addElementToBeginningOfArray(array, el) {
+  var newArray = [el, ...array];
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, el) {
+array.unshift(el);
+ return array
+ 
+}
+function addElementToEndOfArray(array, element) {
+  var changeArray = [...array, element];
+  return changeArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array
+}
+  
+function removeElementFromBeginningOfArray(array) {
+   var newSlice = array.slice(0);
+   return newSlice
+ }
+>>>>>>> 76a02c6e43679fc41e62ed7c0cbcf0d4d1778672
